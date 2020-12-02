99340f99b34cd4aaa44f8888cfc23b647d65e8bb
Suraj Thapa <stsurajst@gmail.com>
2020-12-02 23:17:53 +0545

    Gatsby Data with GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Full-Stack Bootcamp",
+    author: "Suraj Thapa",
+  },
   plugins: [`gatsby-plugin-sass`],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import HeaderStyles from "./header.module.scss"
 
 const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <header className={HeaderStyles.header}>
       <h1>
         <Link to="/" className={HeaderStyles.title}>
-          Suraj Thapa
+          {data.site.siteMetadata.title}
         </Link>
       </h1>
       <nav>
