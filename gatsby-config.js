bb0963949e0e9a782ccf449d6e4348eb0d9b5ad5
Suraj Thapa <stsurajst@gmail.com>
2020-12-29 10:02:07 +0545

    md files added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,14 @@ module.exports = {
     title: "Full-Stack Bootcamp",
     author: "Suraj Thapa",
   },
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
