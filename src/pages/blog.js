e35fe777eecfb7adc213f08134fcfc6470b21d94
Suraj Thapa <stsurajst@gmail.com>
2021-01-01 11:40:16 +0545

    wroking with markdown

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,36 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 
 const Blogpage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log("data>>", data)
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Post will show here later on</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
