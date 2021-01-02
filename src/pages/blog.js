f32c51f296e93128721f3105fea19a93f3327cfb
Suraj Thapa <stsurajst@gmail.com>
2021-01-02 23:42:38 +0545

    image added and css added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Layout from "../components/layout"
+import blogStyle from "./blog.module.scss"
 
 const Blogpage = () => {
   const data = useStaticQuery(graphql`
@@ -24,10 +25,10 @@ const Blogpage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyle.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li>
+            <li className={blogStyle.post}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -5,6 +5,8 @@ date: "2020-04-19"
 
 I just launched a new bootcamp
 
+![Grass](./grass.png)
+
 ## Topics Covered
 
 1. Gatsby
